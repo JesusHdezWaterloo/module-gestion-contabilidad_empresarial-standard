2f5ac771ede29730ecdbfc91c7e8413d1b4226ee
Unknown <unknown@example.com>
2020-10-23 13:20:33 -0500

    ModuleGestionContabilidadEmpresarialConstants: +method,*fix path names

--- src/main/java/com/jhw/module/gestion/contabilidad/core/ModuleGestionContabilidadEmpresarialConstants.java
+++ src/main/java/com/jhw/module/gestion/contabilidad/core/ModuleGestionContabilidadEmpresarialConstants.java
@@ -5,14 +5,24 @@
  */
 package com.jhw.module.gestion.contabilidad.core;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  *
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
 public class ModuleGestionContabilidadEmpresarialConstants {
 
-    public static final String PATH_CUADRE_GENERAL = "/cuadre";
-    public static final String PATH_FIND_ALL_PENDING = "/find_all_pending";
-    public static final String PATH_FIND_ALL_LIQUIDADAS = "/find_all_liquidadas";
-    public static final String PATH_FIND_BY_LIQUIDADA = "/find_by_liquidada";
+    public static final String CUADRE_GENERAL_PATH = "/cuadre";
+    
+    public static final String CUADRE_FIND_ALL_PENDING_PATH = "/find_all_pending";
+    public static final RequestMethod CUADRE_FIND_ALL_PENDING_METHOD = RequestMethod.GET;
+    
+    public static final String CUADRE_FIND_ALL_LIQUIDADAS_PATH = "/find_all_liquidadas";
+    public static final RequestMethod CUADRE_FIND_ALL_LIQUIDADAS_METHOD = RequestMethod.GET;
+    
+    public static final String LIQUIDADA = "liquidada";
+    public static final String CUADRE_FIND_BY_LIQUIDADA_PATH = "/find_by_liquidada/{" + LIQUIDADA + "}";
+    public static final RequestMethod CUADRE_FIND_BY_LIQUIDADA_METHOD = RequestMethod.GET;
+
 }
